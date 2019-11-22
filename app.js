83c7a1972acce28629aea84fb1c2f8b686b6ea1c
lagrange0125@gmail.com <lagrange0125@gmail.com>
2019-11-22 15:00:59 -0500

    added database connection

--- app.js
+++ app.js
@@ -22,6 +22,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
 
+//--------------------------
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to database.');
+});
+
+connection.end();
+//--------------------------------
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
