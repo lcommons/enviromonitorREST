d8f2a97435635a977efe3c3a1948a59d5d738622
lagrange0125@gmail.com <lagrange0125@gmail.com>
2019-11-22 16:23:08 -0500

    removing Jade

--- routes/observation.js
+++ routes/observation.js
@@ -1,5 +1,15 @@
+var mysql = require('mysql');
+const connection = {
+	host: "localhost",
+	user: 'datauser',
+	password: 'localdatauser',
+	database : 'environment_data'
+};
+
+
 exports.index = function(req, res) {
- res.render('observation', {title: 'Weather/Air Observations'});
+ //res.render('observation', {title: 'Weather/Air Observations'});
+    res.send("observations");
 };
 
 exports.add_observation = function(req, res) {
