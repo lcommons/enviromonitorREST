628b164cbe67d786cba6bdb09fbf081856c09912
lagrange0125@gmail.com <lagrange0125@gmail.com>
2019-11-22 16:45:12 -0500

    fixing database config

--- routes/observation.js
+++ routes/observation.js
@@ -6,10 +6,11 @@ var mysql = require('mysql');
 //	database : 'environment_data'
 //};
 var connection = mysql.createConnection({
-  host     : process.env.RDS_HOSTNAME,
-  user     : process.env.RDS_USERNAME,
-  password : process.env.RDS_PASSWORD,
-  port     : process.env.RDS_PORT
+    host     : process.env.RDS_HOSTNAME,
+    user     : process.env.RDS_USERNAME,
+    password : process.env.RDS_PASSWORD,
+    port     : process.env.RDS_PORT,
+    database : 'environment_data'
 });
 
 
